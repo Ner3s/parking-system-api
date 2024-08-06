7e1bb6fb97008192871a71532e972be956a67271
Alan Neres <neres@NEREPC>
2024-08-06 00:37:58 -0300

    refactor: re-init app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { typeOrmAsyncConfig } from './configs/typeorm.config';
-import { UsersModule } from './modules/users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-    UsersModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
